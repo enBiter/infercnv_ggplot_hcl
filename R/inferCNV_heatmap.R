d0a4836b3cc55a6202747063cd1c9db796796d58
Dmitry Kazantsev <32875873+enBiter@users.noreply.github.com>
2023-07-26 13:15:34 +0200

    Update inferCNV_heatmap.R

--- R/inferCNV_heatmap.R
+++ R/inferCNV_heatmap.R
@@ -8,7 +8,7 @@
 
 # shameless copy of dst53/infercnv_edit
 gg_color_hue <- function(n) {
-  hues = seq(15, 375, length = n + 1)
+  hues = seq(15, 275, length = n + 1)
   hcl(h = hues, l = 65, c = 100)[1:n]
 }
 
